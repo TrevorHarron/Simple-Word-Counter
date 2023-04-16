1954c44698c514d18351f63b1651d2eda64bfedb
Trevor Harron <trharron@gmail.com>
2023-04-16 14:32:10 -0700

    init

---
+++ main.cs
@@ -0,0 +1,25 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class main : Control
+{
+	private Dictionary<string, int> counts;
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_find_pressed()
+	{
+
+	}
+
+	public void _on_load_button_pressed()
+	{
+
+	}
+}
